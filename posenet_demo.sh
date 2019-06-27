9011abbf46f6d924f7b8d738763e7a0903eb2aca
tedbrunell <tbrunell@redhat.com>
2019-06-27 12:39:28 -0400

    Modify script to commit the final container into the local registry so that it can be used by Podman

--- posenet_demo.sh
+++ posenet_demo.sh
@@ -10,8 +10,11 @@ buildah --user root run $container bash -c 'npm install @tensorflow/tfjs'
 
 buildah --user root run $container bash -c 'npm install @tensorflow-models/posenet'
 
-buildah copy $container '/PATH_TO/posenet/files/' '/opt/app-root/src/node_modules/@tensorflow-models/posenet'
+buildah copy $container '/home/tbrunell/posenet/files/' '/opt/app-root/src/node_modules/@tensorflow-models/posenet'
 
 buildah --user root run $container bash -c 'chmod +x /opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh'
 
-buildah --user root run $container bash -c '/opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh'
+buildah config --user root --entrypoint '/opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh' $container
+
+buildah commit $container posenet_demo
+
