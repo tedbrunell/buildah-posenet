c79b649972fdf8b68c93401977948ccee6894e10
Ted Brunell <tbrunell@tbrunell.corp.redhat.com>
2019-07-03 22:40:56 -0400

    updated script

--- posenet_demo.sh
+++ posenet_demo.sh
@@ -2,19 +2,23 @@
 
 container=$(buildah from ubi7/nodejs-8)
 
-buildah --user root run $container bash -c 'npm install yarn'
+buildah run $container bash -c 'npm install yarn'
 
-buildah --user root run $container bash -c 'npm install rollup'
+buildah run $container bash -c 'npm install rollup'
 
-buildah --user root run $container bash -c 'npm install @tensorflow/tfjs'
+buildah run $container bash -c 'npm install @tensorflow/tfjs'
 
-buildah --user root run $container bash -c 'npm install @tensorflow-models/posenet'
+buildah run $container bash -c 'npm install @tensorflow-models/posenet'
 
-buildah copy $container '/home/tbrunell/posenet/files/' '/opt/app-root/src/node_modules/@tensorflow-models/posenet'
+buildah copy $container '/PATH_TO/posenet/files/' '/opt/app-root/src/node_modules/@tensorflow-models/posenet'
 
-buildah --user root run $container bash -c 'chmod +x /opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh'
+buildah run $container bash -c 'chmod +x /opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh'
 
-buildah config --user root --entrypoint '/opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh' $container
+buildah config --entrypoint '/opt/app-root/src/node_modules/@tensorflow-models/posenet/yarn.sh' $container
 
 buildah commit $container posenet_demo
+
+echo "If all went well you should be able to run"
+echo "the posenet container."
+echo "sudo podman run -name posenet_demo -it -p 1234:1234 posenet_demo"
 
